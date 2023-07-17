f0f0da4d3ba1b37c704fc0c7de5190bd5834d9df
GabrielMCC2020 <gabrielmcc2002cultural@gmail.com>
2023-07-16 19:36:49 -0500

    segundo commit del proyecto

--- app.js
+++ app.js
@@ -3,10 +3,28 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
+})
+
+app.post('/', (req, res) => {
+    res.send('Got a POST request')
+})
+
+app.put('/user', (req, res) => {
+    res.send('Got a PUT request at /user')
+})
+
+app.delete('/user', (req, res) => {
+    res.send('Got a DELETE request at /user')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
 
+app.use(express.static('public'))
+app.use(express.static('files'))
+app.use('/static', express.static('public'))
+
+const path = require('path')
+app.use('/static', express.static(path.join(__dirname, 'public')))
